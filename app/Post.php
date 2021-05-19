25b47e9391623163af2fab47df95c727c0e189ed
yuta3737 <yuta120623@gmail.com>
2021-05-20 01:16:48 +0900

    8-2の途中

--- app/Post.php
+++ app/Post.php
@@ -4,7 +4,14 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
+
+
 class Post extends Model
 {
-    //
+    public function getByLimit(int $limit_count = 4)
+{
+    // updated_atで降順に並べたあと、limitで件数制限をかける
+    return $this->orderBy('updated_at', 'DESC')->limit($limit_count)->get();
 }
+}
+
