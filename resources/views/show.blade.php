00180066ec01ae1cebdf69a74c00cfa7fe3600a8
yuta3737 <yuta120623@gmail.com>
2021-05-23 03:37:07 +0900

    8-2 css断念

--- resources/views/show.blade.php
+++ resources/views/show.blade.php
@@ -6,7 +6,7 @@
         <title>Posts</title>
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
-        <link rel="stylesheet" href="/css/app.css">
+        ><link rel="stylesheet" href="/css/app.css">
     </head>
     <body>
         <h1 class="title">
