a7876a9de4d56cb1b7f47f17d7e0957eae847623
yuta3737 <yuta120623@gmail.com>
2021-05-23 03:25:35 +0900

    8-2最低限

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -11,5 +11,18 @@ public function index(Post $post)
 {
     return view('index')->with(['posts' => $post->getPaginateByLimit(5)]);
 } 
+
+/**
+ * 特定IDのpostを表示する
+ *
+ * @params Object Post // 引数の$postはid=1のPostインスタンス
+ * @return Reposnse post view
+ */
+public function show(Post $post)
+{
+    return view('show')->with(['post' => $post]);
 }
+}
+
+
 ?>
--- routes/web.php
+++ routes/web.php
@@ -13,3 +13,4 @@
 
 Route::get('/posts', 'PostController@index');
 Route::get('/', 'PostController@index');
+Route::get('/posts/{post}', 'PostController@show');
