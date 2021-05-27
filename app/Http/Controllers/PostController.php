7884bf88b7323214ac80d57c4fb790562469f397
yuta3737 <yuta120623@gmail.com>
2021-05-27 14:36:55 +0900

    8-4 終了2

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -45,7 +45,20 @@ public function edit(Post $post)
         return view('edit')->with(['post' => $post]);
     }
 
+
+public function update(PostRequest $request, Post $post)
+{
+    $input_post = $request['post'];
+    $post->fill($input_post)->save();
+
+    return redirect('/posts/' . $post->id);
 }
+
+
+
+
+}
+
 
 
 
--- resources/views/edit.blade.php
+++ resources/views/edit.blade.php
@@ -13,13 +13,16 @@
             <div class='content__title'>
                 <h2>タイトル</h2>
                 <input type='text' name='post[title]' value="{{ $post->title }}">
+                <p class="title__error" style="color:red">{{ $errors->first('post.title') }}</p>
             </div>
             <div class='content__body'>
                 <h2>本文</h2>
                 <input type='text' name='post[body]' value="{{ $post->body }}">
+                <p class="body__error" style="color:red">{{ $errors->first('post.body') }}</p>
             </div>
             <input type="submit" value="保存">
         </form>
+        <div class="back">[<a href="../">back</a>]</div>
     </div>
 </body>
 </html>
